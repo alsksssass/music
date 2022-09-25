55b13dffd794b650e1e99dfebb504476667575e5
sanghoon <hoone0802@gmail.com>
2022-09-26 00:30:32 +0900

    15초증가

--- index.js
+++ index.js
@@ -91,8 +91,19 @@ if(!client.commands.has(command)) return
 if(admin.readynum == 1){
 if(message.author.bot) return;
 const channel = client.channels.cache.get('1022125527118663700');
-await channel.send("10초안에 모두 준비를 마쳐주세요")
+await channel.send("15초안에 모두 준비를 마쳐주세요")
 await channel.send({ content: ">>> "+admin.class1+"역"+"\n"+admin.player1+"님"+"\n"+"\n"+admin.class2+"역"+"\n"+admin.player2+"님"+"\n"+"\n"+admin.class3+"역"+"\n"+admin.player3+"님"+"\n"+"\n"+admin.class4+"역"+"\n"+admin.player4+"님"+"\n"+"\n"+admin.class5+"역"+"\n"+admin.player5+"님"+"\n"+"\n"})
+await wait(1000);
+await channel.send("14초안에 모두 준비를 마쳐주세요")
+await wait(1000);
+await channel.send("13초안에 모두 준비를 마쳐주세요")
+await wait(1000);
+await channel.send("12초안에 모두 준비를 마쳐주세요")
+await wait(1000);
+await channel.send("11초안에 모두 준비를 마쳐주세요")
+await wait(1000);
+await channel.send("10초안에 모두 준비를 마쳐주세요")
+await wait(1000);
 await channel.send("9초안에 모두 준비를 마쳐주세요")
 await wait(1000);
 await channel.send("8초안에 모두 준비를 마쳐주세요")
